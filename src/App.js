a42400c4e2fe8cc53f37a73455a915250eb0c03f
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-15 15:16:29 +0100

    add switch idzie do pierwszego dziecka

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+// lesson 242/243/244/
 function App() {
   return (
     <Router>
-      <Route  path="/" exact={true}>
-        <Dashboard></Dashboard>
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
     </Router>
   );
 }
