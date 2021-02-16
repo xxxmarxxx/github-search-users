d1efa72462a50200c69e01102c31602b6ebbc797
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-16 16:40:31 +0100

    copy and paste from ExampleChart

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React from "react";
+// import ReactDOM from "react-dom"; nie uzywany
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
-};
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
 
-export default Doughnut2d;
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+
+const ChartComponent = ({data}) => {
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: "Languages",
+        //Set the theme for your chart
+        theme: "fusion",
+        // Number of decimal places to which all numbers on the chart will be rounded to.
+        decimals:0,
+        
+        pieRadius:"45%",
+        // paletteColors:'#f0db4f'
+       
+      },
+      // Chart Data
+      data,
+    }
+  };
+  return <ReactFC {...chartConfigs} />
+}
+
+// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
+// class App extends React.Component {
+//   render() {
+//     return (<ReactFC {...chartConfigs} />);
+//   }
+// }
+
+export default ChartComponent;
