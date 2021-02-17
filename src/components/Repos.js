4f4a9f4e868297dbe7208b1cc1359ec8e2cf69f1
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-17 13:31:00 +0100

    add let {stars,forks}

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -37,8 +37,28 @@ const Repos = () => {
     })
     .map((item) => {
       return { ...item, value: item.stars };
-    }).slice(0,5);
-  console.log(mostPopular);
+    })
+    .slice(0, 5);
+  // console.log(mostPopular);
+
+  // stars, forks
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = {label:name,value:forks}
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+  // console.log(stars)
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
 
   const chartData = [
     {
@@ -59,9 +79,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <Column3D data={chartData}/>
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-       <Bar3D data={chartData}/>
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
