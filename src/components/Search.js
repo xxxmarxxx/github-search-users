0217a72f66398609ef679bcbbca70278d7747edd
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-17 15:53:09 +0100

    add error

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,8 @@ import { GithubContext } from "../context/context";
 
 const Search = () => {
   const [user, setUser] = React.useState("");
-  const { requests } = React.useContext(GithubContext);
-  console.log(requests);
+  const { requests, error } = React.useContext(GithubContext);
+  // console.log(requests);
 
   // get things from global context
   const handleSubmit = (e) => {
@@ -21,6 +21,11 @@ const Search = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch />
