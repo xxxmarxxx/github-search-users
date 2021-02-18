1199a4519bbdc8088976806ff3945d9b35c67f2b
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-18 13:28:46 +0100

    add searchGithubUser, isLoading

--- src/context/context.js
+++ src/context/context.js
@@ -16,12 +16,26 @@ const GithubProvider = ({ children }) => {
   const [followers, setFollowers] = useState(mockFollowers);
   // request loading
   const [requests, setRequests] = useState(0);
-  const [loading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   // error
   const [error, setError] = useState({ show: false, msg: "" });
 
   const searchGithubUser = async (user) => {
-    console.log(user);
+    // console.log(user);
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    // console.log(response);
+    if (response) {
+      setGithubUser(response.data);
+      // more logic here
+    } else {
+      toggleError(true, "there is no user with that username");
+    }
+    checkRequests()
+    setIsLoading(false)
   };
 
   // check rate
@@ -57,6 +71,7 @@ const GithubProvider = ({ children }) => {
         requests,
         error,
         searchGithubUser,
+        isLoading,
       }}
     >
       {children}
