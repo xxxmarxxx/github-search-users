db77caa1fe651a904478da9c6d7401ad26e39329
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-19 16:37:29 +0100

    cleaning

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -47,7 +47,7 @@ const Repos = () => {
     (total, item) => {
       const { stargazers_count, name, forks } = item;
       total.stars[stargazers_count] = { label: name, value: stargazers_count };
-      total.forks[forks] = {label:name,value:forks}
+      total.forks[forks] = { label: name, value: forks };
       return total;
     },
     {
