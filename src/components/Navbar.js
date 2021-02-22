4108c1f3373d75f518aadff9d7c173fcedca4d18
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-22 12:12:19 +0100

    add login and logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,12 +8,30 @@ const Navbar = () => {
     loginWithRedirect,
     logout,
     user,
-    isLodading,
+    isLoading,
   } = useAuth0();
+  const isUser = isAuthenticated && user;
+
+  console.log(isAuthenticated, user, isLoading);
   return (
     <Wrapper>
-      <button onClick={loginWithRedirect}>login</button>
-      <button onClick={()=>{logout}}>logout</button>
+      {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
+      {isUser ? (
+        <button
+          onClick={() => {
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button onClick={loginWithRedirect}>login</button>
+      )}
     </Wrapper>
   );
 };
